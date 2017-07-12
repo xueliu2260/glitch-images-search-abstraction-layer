56901d0087dcff4cf1ccb8b83734d116b26bd471
System <none>
2017-07-12 01:00:03 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -27,28 +27,28 @@ const GoogleImages = require('google-images');
 
 const client = new GoogleImages('017999513681578927553:hwq-m42zzwq', 'AIzaSyDzJHUKZc1WuqYczCjCU7KZ5SYsxHTd7e0');
 
-client.search('Steve Angello')
-	.then(images => {
-		/*
-		[{
-			"url": "http://steveangello.com/boss.jpg",
-			"type": "image/jpeg",
-			"width": 1024,
-			"height": 768,
-			"size": 102451,
-			"thumbnail": {
-				"url": "http://steveangello.com/thumbnail.jpg",
-				"width": 512,
-				"height": 512
-			}
-		}]
-		 */
-	});
+// client.search('Steve Angello')
+// 	.then(images => {
+// 		/*
+// 		[{
+// 			"url": "http://steveangello.com/boss.jpg",
+// 			"type": "image/jpeg",
+// 			"width": 1024,
+// 			"height": 768,
+// 			"size": 102451,
+// 			"thumbnail": {
+// 				"url": "http://steveangello.com/thumbnail.jpg",
+// 				"width": 512,
+// 				"height": 512
+// 			}
+// 		}]
+// 		 */
+// 	});
 
-// paginate results
-client.search('Steve Angello', {page: 2}).then(function(images){
+// // paginate results
+// client.search('Steve Angello', {page: 2}).then(function(images){
   
-});
+// });
 
 var mongodb = require('mongodb');
 
@@ -74,11 +74,18 @@ if (err) {
 
   }
 })
-var querySearch;
-var queryPage;
+var querySearch = "";
+var queryPage = 1;
+
 app.get("/api/imagesearch/:str", function (request, response) {
-  var querySearch = request.params.str;
-  
+  var 
+  querySearch = request.params.str;
+  queryPage = parseInt(getAllUrlParams(request.url).offset);
+  // console.log(querySearch);
+  // console.log(queryPage);
+  client.search(querySearch, {page: queryPage}).then(function(images){
+    console.log(images.length);
+  });
 });
 
   
@@ -90,7 +97,67 @@ var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
+function getAllUrlParams(url) {
 
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    queryString = queryString.split('#')[0];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i=0; i<arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // in case params look like: list[]=thing1&list[]=thing2
+      var paramNum = undefined;
+      var paramName = a[0].replace(/\[\d*\]/, function(v) {
+        paramNum = v.slice(1,-1);
+        return '';
+      });
+
+      // set parameter value (use 'true' if empty)
+      var paramValue = typeof(a[1])==='undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      paramValue = paramValue.toLowerCase();
+
+      // if parameter name already exists
+      if (obj[paramName]) {
+        // convert value to array (if still string)
+        if (typeof obj[paramName] === 'string') {
+          obj[paramName] = [obj[paramName]];
+        }
+        // if no array index number specified...
+        if (typeof paramNum === 'undefined') {
+          // put the value on the end of the array
+          obj[paramName].push(paramValue);
+        }
+        // if array index number specified...
+        else {
+          // put the value at that index number
+          obj[paramName][paramNum] = paramValue;
+        }
+      }
+      // if param name doesn't exist yet, set it
+      else {
+        obj[paramName] = paramValue;
+      }
+    }
+  }
+
+  return obj;
+}
 
 
 
